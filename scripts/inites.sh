7056213ba395452a95ce28846127ae1c9d21dde8
Tien Do <tiendq@gmail.com>
2019-11-14 10:49:46 +0700

    chore: add scripts to create ES index and mapping for movies collection

---
+++ scripts/inites.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+curl -X PUT http://localhost:9200/mflix_movies
+
+curl -X PUT http://localhost:9200/mflix_movies/_mapping -H 'Content-Type: application/json' -d \
+'{
+  "properties": {
+    "plot": {
+      "type": "text"
+    },
+    "genres": {
+      "type": "keyword"
+    },
+    "cast": {
+      "type": "keyword"
+    },
+    "title": {
+      "type": "text"
+    },
+    "fullplot": {
+      "type": "text"
+    },
+    "countries": {
+      "type": "keyword"
+    },
+    "released": {
+      "type": "date",
+      "format": "strict_date"
+    },
+    "directors": {
+      "type": "text"
+    },
+    "year": {
+      "type": "short"
+    },
+    "imdb_rating": {
+      "type": "scaled_float",
+      "scaling_factor": 10
+    },
+    "languages": {
+      "type": "text"
+    },
+    "writers": {
+      "type": "text"
+    },
+    "poster": {
+      "type": "text"
+    }
+  }
+}'
